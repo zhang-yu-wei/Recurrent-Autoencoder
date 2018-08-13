ecbf8589fcfb85c6fbbc3e40c679c2ac272046a8
1234UU <1611515@mail.nankai.edu.cn>
2018-08-13 11:50:20 +0800

    Update autoencoder.py

--- autoencoder.py
+++ autoencoder.py
@@ -10,10 +10,9 @@ class autoencoder(object):
     Class that encapsulates the encoder-decoder architecture
     """
 
-    def __init__(self, hidden_size, num_d_layers, go, train=True, bidirectional=True):
+    def __init__(self, hidden_size, num_d_layers, go, train=True):
         """
                 Initialize the encoder/decoder and creates Tensor objects
-
                 :param hidden_size: hidden layer size of that lstm
                 :param go: index of the GO symbol in the embedding matrix
                 :param train_embeddings: whether to adjust embeddings during training
@@ -25,12 +24,10 @@ class autoencoder(object):
         self.go = go
         self.eos = go
 
-        self.bidirectional = bidirectional
-
         self.global_step = tf.Variable(0, name='global_step', trainable=False)
 
         # the sentence is the object to be memorized
-        self.sentence = tf.placeholder(tf.int32, [None, None, 59, 23], 'sentence')
+        self.sentence = tf.placeholder(tf.int32, [None, None, 59*5], 'sentence')
         self.sentence_size = tf.placeholder(tf.int32, [None],
                                             'sentence_size')
         self.l2_constant = tf.placeholder(tf.float32, name='l2_constant')
@@ -47,7 +44,6 @@ class autoencoder(object):
 
             # encoder cell
             self.lstm_fw = tf.nn.rnn_cell.LSTMCell(hidden_size, initializer=initializer)
-            self.lstm_bw = tf.nn.rnn_cell.LSTMCell(hidden_size, initializer=initializer)
 
             # decoder cell
             cells_d = []
@@ -65,38 +61,23 @@ class autoencoder(object):
             sent_drpot = tf.nn.dropout(self.sentence, self.dropout_keep)
 
             # encoding step
-            if bidirectional:
-                ret = tf.nn.bidirectional_dynamic_rnn(self.lstm_fw, self.lstm_bw, sent_drpot, dtype=tf.float32,
-                                                      sequence_length=self.sentence_size, scope=self.scope)
-            else:
-                ret = tf.nn.dynamic_rnn(self.lstm_fw, sent_drpot, dtype=tf.float32, sequence_length=
-                                        self.sentence_size, scope=self.scope)
+            ret = tf.nn.dynamic_rnn(self.lstm_fw, sent_drpot, dtype=tf.float32, sequence_length=
+                                    self.sentence_size, scope=self.scope)
             _, self.encoded_state = ret
 
-            if bidirectional:
-                encoded_state_fw, encoded_state_bw = self.encoded_state
-
-                # set the scope name used inside the decoder
-                fw_scope_name = self.scope.name + '/fw'
-                bw_scope_name = self.scope.name + '/bw'
-            else:
-                encoded_state_fw = self.encoded_state
-                fw_scope_name = self.scope
+            encoded_state = self.encoded_state
+            fw_scope_name = self.scope
 
             self.scope.reuse_variables()
 
-            # generate a batch of GOs
-            # sentence_size has the batch dimension
-            go_batch = self._generate_batch_go(tf.size(self.sentence_size))
-
-    def _generate_batch_go(self, batch_size):
-        """
-        Generate a 1-d tensor with copies of EOS as big as the batch size
-        :param like: a tensor whose shape the returned embeddings should match
-        :return: a tensor
-        """
-        ones = tf.ones(batch_size, 59, 23)
-        return ones * self.go
+            # decoder_step
+            with tf.variable_scope('decoding') as scope:
+                self.state_d = self.initial_d_state
+                self.output_d, self.state_d = self.stacked_lstm_d(self.encoded_state, self.state_d)
+                self.decode_smiles = self.output_d
+                for i in range(1, self.sentence_size):
+                    self.output_d, self.state_d = self.stacked_lstm_d(self.encoded_state, self.state_d)
+                    self.decode_smiles = tf.concat([self.decode_smiles, self.output_d], 1)
 
 
 
