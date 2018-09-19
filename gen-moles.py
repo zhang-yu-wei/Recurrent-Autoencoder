1ad60cd5573fc3693848b695bad138ca365349ab
zyuwei <1611515@mail.nankai.edu.cn>
2018-09-19 14:22:53 -0400

    Update gen-moles.py

--- gen-moles.py
+++ gen-moles.py
@@ -4,10 +4,8 @@ from collections import Counter
 import numpy as np
 import random
 import argparse
+from progress import progress_timer
 
-"""
-This file is used for generating molecule sentences
-"""
 
 def gen(input, output, max_atm_length, num_exm):
     # define which character is an atom
@@ -15,7 +13,8 @@ def gen(input, output, max_atm_length, num_exm):
     # save atom number of the word at the same time
     dict = {'words': [], 'atm-num': []}
     # load in words
-    with open(input, 'rb') as f:
+    path = input + '.txt'
+    with open(path, 'rb') as f:
         for word in f:
             word = word.decode('utf-8').replace("\n", "")
             dict['words'].append(word)
@@ -38,6 +37,8 @@ def gen(input, output, max_atm_length, num_exm):
         max_num.append(max_atm_length // (i + 1))
     # begin to generate sentences
     sents = []
+    pt = progress_timer(description='generating examples',
+                        n_iter=num_exm)
     while len(sents) < num_exm:
         mole_num = [np.random.randint(max_num[i]) for i in range(len(max_num))]
         total_atm_num = 0
@@ -56,18 +57,19 @@ def gen(input, output, max_atm_length, num_exm):
                     if num_mole < total_mole_num:
                         sent += ' '
             sents.append(sent)
-            print(len(sents))
+            pt.update()
+    pt.finish()
     # save sentences
     text = '\n'.join(sents)
-    path = output + 'txt'
+    path = output + '.txt'
     with open(path, 'ab+') as f:
         f.write(text.encode('utf-8'))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('input', help='Text file previously tokenized '
-                                      '(by whitespace) and preprocessed')
-    parser.add_argument('output', help='save path, do not enter .txt')
+    parser.add_argument('input', help='dictionary with one word on a line, '
+                        'without .txt')
+    parser.add_argument('output', help='save path, without .txt')
     parser.add_argument('-n', help='number of examples',
                         dest='num_exm', default=10000, type=int)
     parser.add_argument('-a', help='maximum atom number in one sentence',
@@ -76,4 +78,3 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     gen(args.input, args.output, args.max_atm_length, args.num_exm)
-
