56c03ec267e308919d9583334b5f72ea8dd9d938
zyuwei <1611515@mail.nankai.edu.cn>
2018-09-19 14:21:40 -0400

    Add files via upload

---
+++ progress.py
@@ -0,0 +1,26 @@
+import progressbar as pb
+
+#define progress timer class
+class progress_timer:
+
+    def __init__(self, n_iter, description="Something"):
+        self.n_iter         = n_iter
+        self.iter           = 0
+        self.description    = description + ': '
+        self.timer          = None
+        self.initialize()
+
+    def initialize(self):
+        #initialize timer
+        widgets = [self.description, pb.Percentage(), ' ',
+                   pb.Bar(marker=pb.RotatingMarker()), ' ', pb.ETA()]
+        self.timer = pb.ProgressBar(widgets=widgets, maxval=self.n_iter).start()
+
+    def update(self, q=1):
+        #update timer
+        self.timer.update(self.iter)
+        self.iter += q
+
+    def finish(self):
+        #end timer
+        self.timer.finish()
